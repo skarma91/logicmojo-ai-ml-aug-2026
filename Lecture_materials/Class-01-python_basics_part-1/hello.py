cd40a092bcf1889a82797e2ae53d035911adfe1e
skarma91 <souravkarmakar29@gmail.com>
2026-08-08 12:03:44 +0530

    8th Aug Lecture Notes

---
+++ Lecture_materials/Class-01-python_basics_part-1/hello.py
@@ -0,0 +1,7 @@
+# Comment - line starting with # is a comment
+
+print("Hello World!")  # This line prints Hello World! to the console
+
+print("this is my first python script") 
+
+print("We are learning ML and AI at LogicMojo")
